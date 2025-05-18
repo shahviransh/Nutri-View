6bc3dd1d785777a77604777fcf3b1786b2f5ed00
Viransh Shah <shahv47@mcmaster.ca>
2025-05-18 19:06:32 -0400

    Update vite and web config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { visualizer } from 'rollup-plugin-visualizer'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), visualizer({
+    filename: 'dist/stats.html',
+    template: 'treemap',
+    gzipSize: true,
+    brotliSize: true,
+  })],
+  envPrefix: ['VITE_'],
 })
